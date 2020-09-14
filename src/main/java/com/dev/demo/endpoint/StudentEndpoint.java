be8d91466b82c4c9d94d9119f867c6b2abe39d4f
Matheus Santana <matheusess@hotmail.com>
2020-09-14 16:31:40 -0400

    Retornando tratamento de erro e status 200 OK no response

--- src/main/java/com/dev/demo/endpoint/StudentEndpoint.java
+++ src/main/java/com/dev/demo/endpoint/StudentEndpoint.java
@@ -1,14 +1,17 @@
 package com.dev.demo.endpoint;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.demo.error.CustomErrorType;
 import com.dev.demo.model.Student;
 import com.dev.demo.util.DateUtil;
 
-import java.util.List;
-import static java.util.Arrays.asList;
 
 import java.time.LocalDateTime;
 
@@ -21,8 +24,20 @@ public class StudentEndpoint {
     
     
     @RequestMapping(method= RequestMethod.GET, path="/list")
-    public List<Student> listAll(){
+    public ResponseEntity<?> listAll(){
         System.out.println("Date formated com injeção de dependência: "+ this.dateUtil.formatLocalDateTimeToDataBasePattern(LocalDateTime.now()));
-        return asList(new Student("Matheus"), new  Student("Emmanoel"));
+        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path="/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable("id") int id){
+        Student student = new Student();
+        student.setId(id);
+        int index = Student.studentList.indexOf(student);
+        if (index == -1)
+            return new ResponseEntity<>(
+                new CustomErrorType("Student Not Found"), HttpStatus.OK
+                );
+        return new ResponseEntity<>(student.studentList.get(index), HttpStatus.OK);
     }
 }
