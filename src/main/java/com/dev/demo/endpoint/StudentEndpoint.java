bee893d95a7f5da3c1ee8f4967d96bda16a5687a
Matheus Santana <matheusess@hotmail.com>
2020-09-17 08:25:17 -0400

    Autenticação de usuário in memory na rota de deleção de Student

--- src/main/java/com/dev/demo/endpoint/StudentEndpoint.java
+++ src/main/java/com/dev/demo/endpoint/StudentEndpoint.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -46,6 +49,7 @@ public class StudentEndpoint {
         return new ResponseEntity<>(student, HttpStatus.OK);
     }
     @DeleteMapping(path = "/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> delete(@PathVariable Long id){
         studentDAO.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
